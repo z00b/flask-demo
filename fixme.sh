19a1d0cacc3cb0283d8de3d1ad11f8c85e648078
Robert Zuber <rob@circleci.com>
2023-07-13 16:26:20 -0700

    living on the edge

--- fixme.sh
+++ fixme.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-fix-it test-results.out hello/hello_app.py
+THE_FILE=hello/hello_app.py
+
+fix-it test-results.out ${THE_FILE}
+git add ${THE_FILE}
+git commit -m "FTFW"
+git push origin HEAD
 exit 1
