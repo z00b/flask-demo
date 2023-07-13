212bc9840758bf009ab4ef4faf702f0399d45e36
Robert Zuber <rob@circleci.com>
2023-07-13 16:40:08 -0700

    git config

--- fixme.sh
+++ fixme.sh
@@ -3,6 +3,8 @@
 THE_FILE=hello/hello_app.py
 
 fix-it test-results.out ${THE_FILE}
+git config --global user.email "rob@zuber.net"
+git config --global user.name "Robert Zuber"
 git add ${THE_FILE}
 git commit -m "FTFW"
 git push origin HEAD
