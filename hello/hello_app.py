3e12813e4c9accecb55515553b9ac3de474a87d4
Robert Zuber <rob@zuber.net>
2023-07-14 02:23:19 +0000

    CircleCI Suggested Fixes

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -7,7 +7,7 @@ APP = Flask(__name__)
 @APP.route("/")
 def hello():
     '''Ensure that people feel very welcome.'''
-    return "Hola, mis amigos!"
+    return "Hello, my friends!"
 
 @APP.route('/goodbye')
 def goodbye():
